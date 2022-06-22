502e66a3861b441b10a2b5e1ce9830b4d53aabf0
tahsinkarim <tahsinnkarim@gmail.com>
2022-06-22 22:18:06 +0600

    Added Pokegame Component

---
+++ src/Pokecard.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import "./index.css";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
+
+export class Pokecard extends Component {
+  render() {
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
+    return (
+      <div className='flex flex-col justify-center items-center bg-gray-300 w-44 rounded-xl px-3 py-3 shadow-lg'>
+        <h2 className='text-blue-700 font-bold text-lg '>{this.props.name}</h2>
+        <img src={imgSrc} alt={this.props.name} />
+        <p>Type: {this.props.type}</p>
+        <p>EXP: {this.props.exp}</p>
+      </div>
+    );
+  }
+}
---
+++ src/Pokedex.jsx
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import { Pokecard } from "./Pokecard";
+export default class Pokedex extends Component {
+  static defaultProps = {
+    pokemon: [
+      { id: 4, name: "Charmander", type: "fire", base_experience: 62 },
+      { id: 7, name: "Squirtle", type: "water", base_experience: 63 },
+      { id: 11, name: "Metapod", type: "bug", base_experience: 72 },
+      { id: 12, name: "Butterfree", type: "flying", base_experience: 178 },
+      { id: 25, name: "Pikachu", type: "electric", base_experience: 112 },
+      { id: 39, name: "Jigglypuff", type: "normal", base_experience: 95 },
+      { id: 94, name: "Gengar", type: "poison", base_experience: 225 },
+      { id: 133, name: "Eevee", type: "normal", base_experience: 65 },
+    ],
+  };
+  render() {
+    return (
+      <div className='Pokedex'>
+        <h1 className='text-2xl text-gray-400 font-bold text-center my-6'>
+          Pokedex
+        </h1>
+        <p className='text-center'>Total Experince: {this.props.exp}</p>
+        <p className='text-center'>
+          {this.props.isWinner ? "WINNER!" : "BETTER LUCK NEXT TIME"}
+        </p>
+        <div className='flex flex-wrap justify-center gap-16 mx-auto my-8 max-w-screen-lg'>
+          {this.props.pokemon.map((p) => (
+            <Pokecard
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
